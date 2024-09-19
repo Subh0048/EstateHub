6a940eff55328e569a7b28719bb9d1f67c4f36f6
Subh0048 <subhamkumarsahu99@gmail.com>
2024-09-19 18:33:25 +0530

    chat section designed

---
+++ client/Estateclientside/src/components/Chat/Chat.jsx
@@ -0,0 +1,86 @@
+
+import "./Chat.scss";
+function Chat() {
+
+
+  return (
+    <div className="chat">
+      <div className="messages">
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <span>subham sahu</span>
+          <p>Lorem Impedit....</p>
+        </div>
+
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <span>subham sahu</span>
+          <p>Lorem Impedit....</p>
+        </div>
+
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <span>subham sahu</span>
+          <p>Lorem Impedit....</p>
+        </div>
+
+        <div className="message">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+
+          <span>subham sahu</span>
+          <p>Lorem Impedit....</p>
+        </div>
+      </div>
+
+      
+
+      <div className="chatbox">
+        <div className="top">
+            <div className="user">
+                <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                subham sahu
+            </div>
+            <span className="close">X</span>
+        </div>
+        <div className="center">
+            <div className="chatmessage">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <span>1 hour ago</span>
+            </div>
+            <div className="chatmessage own">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <span>1 hour ago</span>
+            </div>
+            <div className="chatmessage">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <span>1 hour ago</span>
+            </div>
+            <div className="chatmessage own">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <span>1 hour ago</span>
+            </div>
+        </div>
+        <div className="bottom">
+            <textarea ></textarea>
+            <button>send</button>
+
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
